3a45320844d1fe76109d802430139b5b96487b44
marcos <marcos.fernan.vaz@gmail.com>
2018-05-31 19:29:08 +0200

    Initial commit with react-create-app and reactivesearch

---
+++ elastic-searching/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { ReactiveBase } from '@appbaseio/reactivesearch';
+
+class App extends Component {
+
+  render() {
+    return (
+      <ReactiveBase
+        app="car-store"
+        credentials="cf7QByt5e:d2d60548-82a9-43cc-8b40-93cbbe75c34c">
+        // other components will go here.
+        <div>
+          Hello ReactiveSearch!
+				</div>
+      </ReactiveBase>
+    );
+  }
+}
+
+export default App;
